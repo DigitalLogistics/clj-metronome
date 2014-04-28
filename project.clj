a2c8ab2ca41a418df70939465c4e08042ccbd455
Kris Jenkins <krisajenkins@gmail.com>
2014-04-28 14:02:43 +0100

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-metronome "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
+                 [org.clojure/core.match "0.2.1"]
+                 [http-kit "2.1.16"]
+                 [prismatic/schema "0.2.1"]])
---
+++ src/clj_metronome/core.clj
@@ -0,0 +1,6 @@
+(ns clj-metronome.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
